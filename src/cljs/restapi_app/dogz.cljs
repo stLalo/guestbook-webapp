890bdbcaf7fdc086d4a8157b9bb78e6c7fdaa226
Eduardo Mata <eduardo@2da.us>
2019-03-11 20:19:44 -0500

    adding katz and dogz

---
+++ src/cljs/restapi_app/dogz.cljs
@@ -0,0 +1,40 @@
+(ns restfulapp.dogz 
+    (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET POST]]
+))
+
+
+(defonce links (atom nil))
+
+(defn get-dogz! []
+    (GET "/dogz-pics"
+        {
+            :params {:picCount 10}
+            :handler #(reset! links (vec %))
+        }
+    )
+)
+
+
+(defn make-dogz []
+    [:ul
+        (for [dog @links]
+            ^{:key dog}
+            [:li 
+                [:img {src dog}]]
+        )
+    ]
+)
+
+
+
+(defn get-dogz []
+    (get-dogz)
+    [:div.container
+        [:div.row
+            [:div.col
+                [make-dogz]
+            ]
+        ]
+    ]
+)
---
+++ src/cljs/restapi_app/katz.cljs
@@ -0,0 +1,42 @@
+(ns restfulapp.katz
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET POST]]
+            )
+)
+
+
+
+(defonce links (atom nil))
+
+
+(defn get-katz! []
+  (GET "/api/katz-pics"
+    {
+      :params {:picCount 5}
+      :handler #(reset! links (vec  % ))
+    }
+  )
+)
+
+(defn make-katz []
+ [:ul.content
+   (for [link @links]
+    ^{:key link}
+     [:li
+      [:img {:src (str  link )}]
+      ]
+    )
+  ])
+
+
+(defn katz-page []
+    (get-katz!)
+        [:div.container
+            [:h2 "Katz!"]
+            [make-katz]
+        ]
+        
+)
+
+
+(reagent/render [#'katz-page] (.getElementById js/document "katz"))
